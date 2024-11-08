2b3dd8e6502cebe6f607b6bf18b0f57b2e83a18d
Ruben Gomez <rugobal@gmail.com>
2024-11-08 19:26:37 +0100

    chaning mapping of aerial to toggle it with leader+m

--- .config/nvim/lua/plugins/aerial.lua
+++ .config/nvim/lua/plugins/aerial.lua
@@ -1,25 +1,26 @@
 return {
-  'stevearc/aerial.nvim',
-  opts = {},
-  -- Optional dependencies
-  dependencies = {
-     "nvim-treesitter/nvim-treesitter",
-     "nvim-tree/nvim-web-devicons"
-  },
-  config = function()
-    require("aerial").setup({
-      -- filter_kind = false,  -- Show all symbol kinds, including variables
-      layout = {
-        default_direction = 'left', -- Sets the Aerial window to open on the left side
-      },
-      -- optionally use on_attach to set keymaps when aerial has attached to a buffer
-      on_attach = function(bufnr)
-        -- Jump forwards/backwards with '{' and '}'
-        vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
-        vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
-      end,
-    })
-    -- You probably also want to set a keymap to toggle aerial
-    vim.keymap.set("n", "<leader>a", "<cmd>AerialToggle!<CR>")
-  end
+	"stevearc/aerial.nvim",
+	opts = {},
+	-- Optional dependencies
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter",
+		"nvim-tree/nvim-web-devicons",
+	},
+	config = function()
+		require("aerial").setup({
+			-- filter_kind = false,  -- Show all symbol kinds, including variables
+			layout = {
+				default_direction = "left", -- Sets the Aerial window to open on the left side
+			},
+			-- optionally use on_attach to set keymaps when aerial has attached to a buffer
+			on_attach = function(bufnr)
+				-- Jump forwards/backwards with '{' and '}'
+				vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
+				vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
+			end,
+		})
+		-- You probably also want to set a keymap to toggle aerial
+		vim.keymap.set("n", "<leader>m", "<cmd>AerialToggle<CR>")
+		-- vim.keymap.set("n", "<leader>m", "<cmd>AerialToggle!<CR>") -- the ! will keep the cursor on the editor buffer
+	end,
 }
